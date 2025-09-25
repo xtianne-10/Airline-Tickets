ba35e0bf0b2b6c6c1ba13698c69d0be26f925a53
xtianne-10 <navarro.christianne@gmail.com>
2025-09-26 01:23:27 +0800

    Try 2

---
+++ Airline-Tickets/src/test/java/com/Airline_Tickets/AirlineTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Airline_Tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
