5e1bfc546dd7b05f2ecc8cb56c7dacd91a6156ad
Windows <Windows@DESKTOP-CESTJAO>
2025-09-30 21:20:22 +0800

    update

--- Airline-Tickets/src/main/java/com/Airline_Tickets/controllers/HomeController.java
+++ Airline-Tickets/src/main/java/com/Airline_Tickets/controllers/HomeController.java
@@ -1,5 +1,15 @@
 package com.Airline_Tickets.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+	
+	@GetMapping("/login")
+	public String index() {
+		return "index.jsp";
+		
+	}
 
 }
