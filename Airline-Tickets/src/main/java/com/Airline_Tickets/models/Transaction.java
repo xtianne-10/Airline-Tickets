8b6d9af5637ac4738509516e1db2c4fdbc1bee79
kc <kc@example.com>
2025-10-05 13:22:33 +0800

    ArrayList - Transaction(kc)

--- Airline-Tickets/src/main/java/com/Airline_Tickets/models/Transaction.java
+++ Airline-Tickets/src/main/java/com/Airline_Tickets/models/Transaction.java
@@ -1,5 +1,22 @@
 package com.Airline_Tickets.models;
 
-public class Transaction {
+import java.util.ArrayList;
+import java.util.UUID;
 
+public class Transaction {
+	  public static String generateUUIDTransactionCode() {
+	        return UUID.randomUUID().toString().toUpperCase();
+	    }
+	    public static void main(String[] args) {
+	       
+	        ArrayList<String> transactionCodes = new ArrayList<>();
+	        for (int i = 0; i < 10; i++) {
+	            String code = generateUUIDTransactionCode();
+	            transactionCodes.add(code);
+	        }
+	        System.out.println("Generated Transaction Codes:");
+	        for (String code : transactionCodes) {
+	            System.out.println(code);
+	        }
+	    }
 }
